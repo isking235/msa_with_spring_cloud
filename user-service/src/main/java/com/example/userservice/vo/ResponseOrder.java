5a74d5f744e7cefd55338e7c0718daf5730c58b2
sangho235 <sangho235@it.gsshop.com>
2022-09-26 15:59:08 +0900

    5장 Catalogs and Orders Microservice - 1~6 사용자 조회

---
+++ user-service/src/main/java/com/example/userservice/vo/ResponseOrder.java
@@ -0,0 +1,14 @@
+package com.example.userservice.vo;
+
+import java.util.Date;
+
+public class ResponseOrder {
+    private  String procductId;
+    private Integer qty;
+    private Integer unitPrice;
+    private Integer totalPrice;
+    private Date createdAt;
+
+    private  String orderId;
+
+}
