b707fd3676c18d7e618a0df1ca5a40aaa00da5cb
sangho235 <sangho235@it.gsshop.com>
2022-10-09 00:19:34 +0900

    7장 Routes 정보 변경

--- user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -1,15 +1,31 @@
 package com.example.userservice.security;
 
+import com.example.userservice.service.UserService;
+import org.apache.catalina.User;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    private UserService userService;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private Environment env;
+
+    public WebSecurity(Environment env, UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.env = env;
+        this.userService = userService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    protected void configure(HttpSecurity http) throws Exception { //권한에 관한 것
         //super.configure(http);
         http.csrf().disable();
         //http.authorizeRequests().antMatchers("/users/**").permitAll(); //이건 login 구현 하면서 주석 처리
@@ -29,5 +45,12 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         authenticationFilter.setAuthenticationManager(authenticationManager());
 
         return authenticationFilter;
+    }
+
+    // select pwd from users where email = ?
+    // db_pwd(encrypted == input_pwd(encrypted)
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception { //인증에 관한 것
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
     }
 }
